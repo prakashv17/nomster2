736664d631aea151e884031c1f9d78d8d929e625
prakashv17 <prakashv17@gmail.com>
2016-06-05 13:13:07 +0000

    Added places to home page

---
+++ db/migrate/20160605122632_create_places.rb
@@ -0,0 +1,13 @@
+class CreatePlaces < ActiveRecord::Migration
+  def change
+    create_table :places do |t|
+
+    	t.string :name
+    	t.string :address
+    	t.text :description
+    	
+
+      t.timestamps
+    end
+  end
+end
